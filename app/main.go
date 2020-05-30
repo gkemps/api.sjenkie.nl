dea4bc19a3c0528f6865e9cf95d9bae760eba4ed
geert <geert@route42.nl>
2020-05-30 23:18:27 +0200

    basic auth

--- app/main.go
+++ app/main.go
@@ -1,10 +1,37 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"os"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+
+	jsonFile, err := os.Open("conf/auth.json")
+	if err != nil {
+		panic(err)
+	}
+	defer jsonFile.Close()
+
+	jsonBytes, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		panic(err)
+	}
+
+	var accounts gin.Accounts
+	err = json.Unmarshal(jsonBytes, &accounts)
+	if err != nil {
+		panic(err)
+	}
+
+	// Group using gin.BasicAuth() middleware
+	// gin.Accounts is a shortcut for map[string]string
+	authorized := r.Group("/", gin.BasicAuth(accounts))
+
+	authorized.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
